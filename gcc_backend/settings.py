d4efb149d989e4c2e8450d12171e22514c282ab9
vishaldubey <vkd2695@gmail.com>
2026-02-04 11:49:21 +0530

    chnges

--- gcc_backend/settings.py
+++ gcc_backend/settings.py
@@ -13,20 +13,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import environ
+import json
 env = environ.Env()
 environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# load_dotenv(os.path.join(BASE_DIR, ".env"))
 
-import json
-ENV = os.getenv('DJANGO_ENV', 'dev')
-if ENV == 'prod':
-    environ.Env.read_env(os.path.join(BASE_DIR, '.env.prod'))
-else:
-    environ.Env.read_env(os.path.join(BASE_DIR, '.env.dev'))
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# ENV = os.getenv('DJANGO_ENV', 'dev')
+# if ENV == 'prod':
+#     environ.Env.read_env(os.path.join(BASE_DIR, '.env.prod'))
+# else:
+#     environ.Env.read_env(os.path.join(BASE_DIR, '.env.dev'))
 
 
 

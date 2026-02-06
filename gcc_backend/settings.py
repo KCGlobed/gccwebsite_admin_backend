aebe8a146b355edd76b7f94e885e1d5bb318f5fc
vishaldubey <vkd2695@gmail.com>
2026-02-06 11:34:28 +0530

    chnges

--- gcc_backend/settings.py
+++ gcc_backend/settings.py
@@ -25,7 +25,8 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env("/.env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/

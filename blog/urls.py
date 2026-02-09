347c62635b17ac803c767210ee4ea5efbd01034f
vishaldubey <vkd2695@gmail.com>
2026-02-09 12:39:57 +0530

    chnges

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("tags_dropdown", BlogTag_dropdown.as_view()),
     path("category_dropdown", BlogCategory_dropdown.as_view()),
 
+
     path("create_blog", CreateBlogView.as_view()),
     path("update_blog/<int:pk>", UpdateBlogView.as_view()),
     path("delete_blog/<int:pk>", DeleteBlogView.as_view()),

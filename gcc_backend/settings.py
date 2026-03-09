1b4cfe12b9de73c2370662eaef9a6238fa73063a
vishaldubey <vkd2695@gmail.com>
2026-03-09 17:30:43 +0530

    chnges

--- gcc_backend/settings.py
+++ gcc_backend/settings.py
@@ -200,8 +200,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=30),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=50),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
 

d8fd3eea7018c31d8e9d396d117a51b0877564d6
vishaldubey <vkd2695@gmail.com>
2026-03-10 17:50:06 +0530

    chnges for commans

--- students/serializers.py
+++ students/serializers.py
@@ -323,7 +323,7 @@ class CompleteStudentSerializer(serializers.ModelSerializer) :
             datas.employement_status = validate_data.get('employement_status', datas.employement_status)
             datas.aadhaar = validate_data.get('aadhaar', datas.aadhaar)
             datas.dob_certificate = validate_data.get('dob_certificate', datas.dob_certificate)
-            datas.photo = validate_data.get('photo', datas.photo)
+            datas.photo = validate_data.get('photo', datas.photo),
             datas.signature = validate_data.get('signature', datas.signature)
             datas.save()
             query = datas
@@ -372,7 +372,7 @@ class CompleteStudentSerializer(serializers.ModelSerializer) :
                 employement_status = validate_data.get('employement_status'),
                 aadhaar = validate_data.get('aadhaar'),
                 dob_certificate = validate_data.get('dob_certificate'),
-                photo = validate_data.get('photo')
+                photo = validate_data.get('photo'),
                 signature = validate_data.get('signature')
 
             )

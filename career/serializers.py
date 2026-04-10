c46014051a3af5b5134d164c02c54aefefcfd93e
vishaldubey <vkd2695@gmail.com>
2026-04-10 17:54:31 +0530

    chnges

--- career/serializers.py
+++ career/serializers.py
@@ -374,6 +374,9 @@ class CreateDossierAbondantSerializer(serializers.ModelSerializer):
                 # "country": "India",
                 "source":m_source,
                 "cf_source":m_source,
+                "cf_utmsource1":instance.utm_source,
+                "medium":instance.utm_medium,
+                "campaign":instance.utm_campaign,
                 "cf_payment_status":"Pending"
             }
 

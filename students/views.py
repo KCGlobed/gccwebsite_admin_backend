d1a3057a8caba8fe86c52d2f4cf86359835cd897
vishaldubey <vkd2695@gmail.com>
2026-07-30 16:02:58 +0530

    chnges

--- students/views.py
+++ students/views.py
@@ -187,11 +187,11 @@ class StudentSourcePayment_list(APIView):
             if str(source_type) == str(SourceType.EAWebsite):
                 access_data = settings.EAUTMSOURCE
                 if str(request.user.first_name).lower() in access_data:
-                    datas = Payments.objects.filter(source=source_type, dossier_form__utm_source=request.user.first_name).order_by('-id')
+                    datas = Payments.objects.filter(source=source_type, dossier_form__utm_source__iexact=str(request.user.first_name).lower()).order_by('-id')
             if str(source_type) == str(SourceType.CPAWebsite):
                 access_data = settings.CPAUTMSOURCE
                 if str(request.user.first_name).lower() in access_data:
-                    datas = Payments.objects.filter(source=source_type, dossier_form__utm_source=request.user.first_name).order_by('-id')
+                    datas = Payments.objects.filter(source=source_type, dossier_form__utm_source__iexact=str(request.user.first_name).lower()).order_by('-id')
         else:
             datas = Payments.objects.filter(source=SourceType.Website).order_by('-id')
 

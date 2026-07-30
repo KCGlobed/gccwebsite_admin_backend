f8bac190c9415d9dc4cad24a2ab3539eca2c5526
vishaldubey <vkd2695@gmail.com>
2026-07-30 18:39:02 +0530

    chnges

--- career/views.py
+++ career/views.py
@@ -1522,11 +1522,11 @@ class GetDossierSourceReportExcelView(APIView):
             if str(source_type) == str(SourceType.EAWebsite):
                 access_data = settings.EAUTMSOURCE
                 if str(request.user.first_name).lower() in access_data:
-                    datas = DossierData.objects.filter(source=source_type, utm_source=request.user.first_name).order_by('-id')
+                    datas = DossierData.objects.filter(source=source_type, utm_source__iexact=str(request.user.first_name).lower()).order_by('-id')
             if str(source_type) == str(SourceType.CPAWebsite):
                 access_data = settings.CPAUTMSOURCE
                 if str(request.user.first_name).lower() in access_data:
-                    datas = DossierData.objects.filter(source=source_type, utm_source=request.user.first_name).order_by('-id')
+                    datas = DossierData.objects.filter(source=source_type, utm_source__iexact=str(request.user.first_name).lower()).order_by('-id')
 
         else:
             datas = DossierData.objects.filter(source=SourceType.Website).order_by('-id')
